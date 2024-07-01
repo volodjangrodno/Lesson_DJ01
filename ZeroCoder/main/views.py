3f32e6deadf1f75e783b91babcb9d469993872fa
volodjangrodno <parthnersclub.ru@gmail.com>
2024-07-01 23:29:57 +0300

    lesson_DJ02klass

--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    # так прописываем если на странице не много информации
+    # return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    return render(request, 'main/index.html', {'caption':'Хомяк-Комбо на Django'}) # а так прописываем если хотим вызвать целую html- страницу, где много информации
 
 def new(request):
-    return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+    # так прописываем если на странице немного информации
+    # return HttpResponse("<h1>Это вторая страница моего проекта на Django</h1>")
+    return render(request, 'main/new.html') # а так прописываем если хотим вызвать целую html- страницу, где много информации
--- ZeroCoder/myotherapp/views.py
+++ ZeroCoder/myotherapp/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+    # так прописываем если на странице немного информации
+    # return HttpResponse("<h1>Это главная страница моего проекта на Django</h1>")
+    return render(request, 'main/index.html') # а так прописываем если хотим вызвать целую html- страницу, где много информации
 
 def data(request):
-    return HttpResponse("<h1>Это страница DATA моего проекта на Django</h1>")
+    # так прописываем если на странице немного информации
+    # return HttpResponse("<h1>Это страница data моего проекта на Django</h1>")
+    return render(request, 'myotherapp/data.html') # а так прописываем если хотим вызвать целую html- страницу, где много информации
+
 def test(request):
-    return HttpResponse("<h1>Это страница TEST моего проекта на Django</h1>")
+    # так прописываем если на странице немного информации
+    # return HttpResponse("<h1>Это  страница test моего проекта на Django</h1>")
+    return render(request, 'myotherapp/test.html') # а так прописываем если хотим вызвать целую html- страницу, где много информации
